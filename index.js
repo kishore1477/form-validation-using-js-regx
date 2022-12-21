23b64cd3a2e49dd3ec52159b1d96fe2a9e121a03
Kishore Kumar <kishorejaipal477@gmail.com>
2022-12-21 04:27:32 +0000

    final code

--- index.js
+++ index.js
@@ -1,26 +1,173 @@
-const formelm =  document.forms.myform
-const email = formelm.email
-const _name = formelm.name
-const password = formelm.password
+const msg = document.getElementsByClassName('msg')
+var msgg = document.getElementById('msgg')
+ 
+ 
+ var name1 = document.getElementById('name')
+ var  email = document.getElementById('email')
+ var  password = document.getElementById('password')
+ var  phone = document.getElementById('phone')
 
-_name.addEventListener('blur', ()=>{
-    console.log("_name is blurred");
+ 
+name1.addEventListener('input', (e)=>{
+ 
+var msgg = document.getElementById('msgg')
+msgg.innerHTML = ""
+    
     // Validate _name here
-    let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
-    let str = _name.value;
-    console.log(regex, str);
+    let regex = /^[a-zA-Z]([0-9a-zA-Z\s]){2,10}$/;
+    let str =  e.target.value;
+    
+    
+    name1.classList.remove('invalid');
+    
+    if(str.length > 0 ){
+
+   
     if(regex.test(str)){
-        console.log('Your _name is valid');
-        _name.classList.remove('is-invalid');
-        validUser = true;
-    }
-    else{
-        console.log('Your _name is not valid');
-        _name.classList.add('is-invalid');
-        validUser = false;
+        // 
+        msgg.innerHTML = ""
+        name1.classList.remove('invalid');
         
     }
-})
+    else{
+        // 
+        if(str.length> 10){
+            msgg.innerHTML = "maximum 10 letters are allowed";
 
-console.log("Email is", email)
-console.log("fromElement",formelm)
+        }else{
+
+            msgg.innerHTML = " name should be 3-10 letters, Example:  name123"
+        }
+ 
+        name1.classList.add('invalid');
+         
+        
+    }
+}else{
+       
+}
+})
+phone.addEventListener('blur', (e)=>{
+ 
+var msgg = document.getElementById('msggPhone')
+msgg.innerHTML = ""
+    
+    // Validate _name here
+    let regex =  /^(\d{03})([0-9]){11}$/;
+    let str =  phone.value;
+    
+    
+    phone.classList.remove('invalid');
+    
+    if(str.length > 0 ){
+
+        phone.classList.remove('invalid');
+    if(regex.test(str)){
+        
+        // 
+        msgg.innerHTML = ""
+        phone.classList.remove('invalid');
+        
+    }
+    else{
+        
+        // 
+        if(str.length> 11){
+            msgg.innerHTML = "Phone must be 11 numbers";
+
+            phone.classList.add('invalid');
+        } else{
+            msgg.innerHTML = "Phone must start with 03 and must be 11 numbers";
+            phone.classList.add('invalid');
+        }
+       
+        
+    }
+}else{
+       
+}
+})
+email.addEventListener('blur', (e)=>{
+ 
+var msgg = document.getElementById('msgEmail')
+msgg.innerHTML = ""
+    
+    // Validate _name here
+    let regex =  /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    let str =  e.target.value;
+    
+    
+    email.classList.remove('invalid');
+    
+    if(str.length > 0 ){
+
+   
+    if(regex.test(str)){
+        // 
+        msgg.innerHTML = ""
+        email.classList.remove('invalid');
+        
+    }
+    else{
+        // 
+        if(str.length> 30){
+            msgg.innerHTML = "maximum 30 letters are allowed";
+
+        }else{
+
+            msgg.innerHTML = " email  should be valid, Example:   example@example.com"
+        }
+ 
+        email.classList.add('invalid');
+         
+        
+    }
+}else{
+       
+}
+})
+password.addEventListener('blur', (e)=>{
+ 
+var msgg = document.getElementById('msgPassword')
+msgg.innerHTML = ""
+    
+    // Validate _name here
+    let regex =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    let str =  e.target.value;
+    
+    
+    if(str.length === 0){
+        password.classList.remove('invalid');
+
+    }
+    
+    if(str.length > 0 ){
+
+   
+    if(regex.test(str)){
+        // 
+        msgg.innerHTML = ""
+        password.classList.remove('invalid');
+        
+    }
+    else{
+        // 
+       if(str.length > 8){
+            msgg.innerHTML = "maximum 8  characters are allowed";
+            password.classList.add('invalid');
+
+        }else{
+            msgg.innerHTML = "must contain at least 1 lower, 1 upper,1 number and 1 special characters"
+ 
+            password.classList.add('invalid');
+       }
+
+           
+         
+        
+    }
+}else{
+       
+}
+})
+ 
